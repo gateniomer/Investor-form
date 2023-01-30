bcd0e46449e1c5dcf0efb7f2b7b7c1f609342abb
gateniomer <gateniomer@gmail.com>
2023-01-30 19:56:56 +0200

    FIXED ROUTING

--- components/header.tsx
+++ components/header.tsx
@@ -2,18 +2,8 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="text-lg flex justify-between max-w-[1600px] mx-auto px-10">
-      <Image
-        src={process.env.NEXT_PUBLIC_GITHUB_PAGES + "/logo.png"}
-        width="100"
-        height="100"
-        alt="logo"
-      />
-      <Image
-        src={process.env.NEXT_PUBLIC_GITHUB_PAGES + "/user.png"}
-        width="100"
-        height="100"
-        alt="logo"
-      />
+      <Image src={"/logo.png"} width="100" height="100" alt="logo" />
+      <Image src={"/user.png"} width="100" height="100" alt="logo" />
     </header>
   );
 }
